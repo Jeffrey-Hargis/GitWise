767d590afcb50a7571a458dcbf30e6dc7dbc4829
Jeffrey Hargis <jeffreyhargis@Jeffreys-MacBook-Pro.local>
2019-03-26 18:58:25 -0400

    GitHub API connected

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import { githubClient } from './services'
+import Auth from './services/auth'
+import { history } from './utils'
 
-
+const auth = new Auth();
 
 ReactDOM.render(
-    <ApolloProvider client={githubClient}>
-        <Router>
-            <App />
+    <ApolloProvider client={githubClient(auth)}>
+        <Router history={history}>
+            <App auth={auth} />
         </Router>
     </ApolloProvider>, 
 document.getElementById('root'));
---
+++ src/services/auth.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { history } from '../utils'
+
+// TODO: Jeff, replace this.
+// TODO: follow the heroku directions https://github.com/prose/gatekeeper here
+// TODO: with your own github oauth shit
+const exchangeUrl = "https://jeffs-stupid-thing.herokuapp.com/authenticate/";
+
+export default class Auth {
+    static TOKEN_KEY = "token";
+
+    async login(code) {
+        try {
+            const { data } = await axios.get(`${exchangeUrl}${code}`);
+            localStorage.setItem(Auth.TOKEN_KEY, data.token);
+            history.replace('/home');
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    logout() {
+        localStorage.clear();
+        history.replace('/');
+    }
+
+    getAccessToken() {
+        return localStorage.getItem(Auth.TOKEN_KEY)
+    }
+
+    isAuthenticated() {
+        return Boolean(localStorage.getItem(Auth.TOKEN_KEY));
+    }
+}
