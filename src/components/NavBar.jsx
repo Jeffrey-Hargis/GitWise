ff36b1a2d182e77b51ba0a166929b7a46c608e14
Jeffrey Hargis <jeffreyhargis@jeffreys-mbp.lan>
2019-04-01 15:35:30 -0400

    Avatar work

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'; 
-import Navbar from 'react-bootstrap/Navbar'
-import logo from '../images/gitwisecat.png';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-import logout from '../services/auth.js';
+import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../images/gitwisecat.png";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import logout from "../services/auth.js";
 
 const USER_QUERY = gql`
   {
@@ -23,28 +23,33 @@ class NavBar extends Component {
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home" className="d-inline-flex">
           <img
-          src={logo}
+            src={logo}
             alt="logo"
             width="60"
             height="50"
             className="d-inline-flex align-top"
           />
           <h2>GITWISE</h2>
-          {this.props.auth.isAuthenticated() ? <Query query={USER_QUERY}>
-            {({ loading, error, data }) => {
-                if (loading) return 'Loading...';
+          {this.props.auth.isAuthenticated() ? (
+            <Query query={USER_QUERY}>
+              {({ loading, error, data }) => {
+                if (loading) return "Loading...";
                 if (error) return null;
-                return <div className="right: 50px">
-                
-                {data.viewer.login} : {data.viewer.starredRepositories.totalCount}
-      
-                </div>
-            }}
-          </Query> : null}
+                return (
+                  <div className="left: 50px">
+                    <div>
+                      {data.viewer.login} :{" "}
+                      {data.viewer.starredRepositories.totalCount}
+                    </div>
+                  </div>
+                );
+              }}
+            </Query>
+          ) : null}
         </Navbar.Brand>
-      </Navbar> 
-    )
+      </Navbar>
+    );
   }
 }
 
-  export default NavBar;  
+export default NavBar;
